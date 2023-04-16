469eabe437d0ff49540a6511dcdc18fa928f2d2f
Gautam Kumarm <g2gautamsingh@gmail.com>
2023-04-16 20:37:34 +0530

    check

--- src/config/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const Connect = async () => {
-  return mongoose.connect(`${process.env.URL}/techlivecall`);
-};
-
-module.exports = Connect;
--- src/route/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { registerUser, userLogin } = require("../controller/user");
-
-const userRouter = express.Router();
-
-userRouter.post("/register", registerUser);
-userRouter.post("/login", userLogin);
-
-module.exports = userRouter;
